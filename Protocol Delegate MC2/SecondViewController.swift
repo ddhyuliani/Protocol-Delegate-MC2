a10c908cad20321dc815a44450b691a8080d461a
Dian DInihari | Tia <55896324+ddhyuliani@users.noreply.github.com>
2022-06-24 00:39:41 +0700

    add comments for clarity

--- Protocol Delegate MC2/SecondViewController.swift
+++ Protocol Delegate MC2/SecondViewController.swift
@@ -18,17 +18,20 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var roomPicker: UIPickerView!
     @IBOutlet weak var selectedLabel: UILabel!
     
+    //picker data dummy
     let roomData = ["Bed Room", "Living Room", "Bath Room"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         roomPicker.dataSource = self
         roomPicker.delegate = self
     }
 
     @IBAction func lightUpButton(_ sender: Any) {
+        
+        //masih force unwrap
+        //when the button is pressed, it will pass the data(job) to the 1st viewcontroller
         selectionDelegate?.didSelectRoom(roomName: selectedLabel.text!, image: UIImage(named: "on")!)
         dismiss(animated: true, completion: nil)
     }
--- Protocol Delegate MC2/ViewController.swift
+++ Protocol Delegate MC2/ViewController.swift
@@ -21,12 +21,13 @@ class ViewController: UIViewController {
     @IBAction func chooseButton(_ sender: Any) {
         let roomSelectionVC = storyboard?.instantiateViewController(withIdentifier: "roomSelection") as! SecondViewController
         
-        
+        //hey selection delegate, you have variable "selection delegate", and I want to do that delegate
         roomSelectionVC.selectionDelegate = self
         present(roomSelectionVC, animated: true, completion: nil)
     }
 }
 
+//conform or comply to the protocol
 extension ViewController: roomSelectionDelegate {
     func didSelectRoom(roomName: String, image: UIImage) {
         roomLabel.text = roomName
